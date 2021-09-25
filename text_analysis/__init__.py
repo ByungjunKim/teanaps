f944f005603cfa74046476b26184df404b6d9a27
김병준 <kuntakim88@gmail.com>
2021-09-25 15:19:51 +0900

    TfidfCalculator, CoWordCalculator만 활성화(나머지 주석)

--- text_analysis/__init__.py
+++ text_analysis/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
 from teanaps.text_analysis.CoWordCalculator import CoWordCalculator
-from teanaps.text_analysis.DocumentSummarizer import DocumentSummarizer
+# from teanaps.text_analysis.DocumentSummarizer import DocumentSummarizer
 from teanaps.text_analysis.TfidfCalculator import TfidfCalculator
-from teanaps.text_analysis.TopicClustering import TopicClustering
-from teanaps.text_analysis.DocumentClustering import DocumentClustering
-from teanaps.text_analysis.SentimentAnalysis import SentimentAnalysis
-from teanaps.text_analysis.KeywordExtractor import KeywordExtractor
-from teanaps.text_analysis.SimCalculator import SimCalculator
+# from teanaps.text_analysis.TopicClustering import TopicClustering
+# from teanaps.text_analysis.DocumentClustering import DocumentClustering
+# from teanaps.text_analysis.SentimentAnalysis import SentimentAnalysis
+# from teanaps.text_analysis.KeywordExtractor import KeywordExtractor
+# from teanaps.text_analysis.SimCalculator import SimCalculator
